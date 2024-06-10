47411577c258a0dd531b68fbe6c5bece56a5ce08
Kader Bozkaya <87239102+kaderbozkaya@users.noreply.github.com>
2024-06-10 13:17:25 +0300

    first commit

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,5 +1,45 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import Badge from "../components/Badge";
+import axios from "axios";
+import { toast } from "react-toastify";
+import { Link, useParams } from "react-router-dom";
+import { MDBContainer, MDBIcon, MDBTypography } from "mdb-react-ui-kit";
 export default function Blog() {
-  return <div>Blog</div>;
+  const [blog, setBlog] = useState();
+  const { id } = useParams();
+  useEffect(() => {
+    if (id) {
+      getSingleBlog();
+    }
+  }, [id]);
+  const getSingleBlog = async () => {
+    const response = await axios.get(`http://localhost:5000/blogs/${id}`);
+    if (response.status === 200) {
+      setBlog(response.data);
+    } else {
+      toast.error("Something went wrong");
+    }
+  };
+
+  return (
+    <>
+      <MDBContainer style={{ border: "1px solid #d1ebe8" }}>
+        <MDBTypography tag="h2" className="mt-2 text-red-600 inline-block">
+          {blog && blog.title}
+        </MDBTypography>{" "}
+        <br />
+        <strong className="flex items-center p-3">
+          <MDBIcon far icon="calendar" />
+          {blog && blog.date}
+          <Badge> {blog && blog.category}</Badge>
+        </strong>
+        <img
+          src={blog && blog.imageUrl}
+          className="rounded w-full max-h-[600px]"
+        />
+        <p className="mt-3 text-left">{Blog && blog.description}</p>
+        <Link to="/">Go Back</Link>
+      </MDBContainer>
+    </>
+  );
 }
